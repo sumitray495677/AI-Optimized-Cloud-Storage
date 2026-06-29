e0aac42889b804efe405f74f62b946405e3c3820
sumitray495677 <sumitray495677@gmail.com>
2026-06-30 01:28:33 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,html}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [],
+  // Enable class-based dark mode (recommended)
+  darkMode: 'class',
+}
+
